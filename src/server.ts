87db0d85294e35b80b588b518659dd5878302525
Awal Hossain <80216813+AwalHossain@users.noreply.github.com>
2023-05-30 05:34:21 +0000

    config ✔️

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,24 @@
 import mongoose from "mongoose";
+import  app  from "./app";
 
   
+
+const port = 5000;
+
+
+
 async function bootstrap(){
 
     try {
         await mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+        app.listen(port, () => {
+            console.log(`Server running on port ${port}`);
+        });
       } catch (error) {
         console.log(error);
       }
+
 }
 
 bootstrap();
