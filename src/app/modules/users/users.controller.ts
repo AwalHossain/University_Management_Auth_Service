970980724d1ed5aebd4f60ebcb83ac3cb0f826b5
Awal Hossain <80216813+AwalHossain@users.noreply.github.com>
2023-06-04 11:41:55 +0000

    [DONE] create user service 🚀🚀

--- src/app/modules/users/users.controller.ts
+++ src/app/modules/users/users.controller.ts
@@ -2,12 +2,19 @@ import { Request, Response } from 'express'
 import usersService from './users.service'
 
 const createUser = async (req: Request, res: Response) => {
-  try {
-    const { user } = req.body
+  const { user } = req.body
+  console.log(user, 'user')
 
+  try {
     const result = await usersService.createUser(user)
 
-    res.status(201).json({ data: result })
+    res
+      .status(201)
+      .json({
+        success: true,
+        message: 'user created successfully!',
+        data: result,
+      })
   } catch (err) {
     res.status(500).json({
       success: false,
