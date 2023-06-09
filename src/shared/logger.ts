04b4843c6143a2d1785073b8e8dec180755b4ecd
Awal Hossain <80216813+AwalHossain@users.noreply.github.com>
2023-06-09 05:44:16 +0000

    daily rotate file ❌

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -1,41 +1,47 @@
 import { createLogger, format, transports } from 'winston'
-const { combine, timestamp, label, prettyPrint, printf } = format
+const { combine, timestamp, label, printf } = format
 import path from 'path'
+import DailyRotateFile from 'winston-daily-rotate-file'
 
 const myFormat = printf(({ level, message, label, timestamp }) => {
   const date = new Date(timestamp)
-  const hours = date.getHours()
+  const hour = date.getHours()
   const minutes = date.getMinutes()
   const seconds = date.getSeconds()
-  const timestampString = `${hours}:${minutes}:${seconds}`
-  return `${timestampString} [${label}] ${level}: ${message}`
+  return `${date.toDateString()} ${hour}:${minutes}:${seconds} } [${label}] ${level}: ${message}`
 })
 
 const logger = createLogger({
   level: 'info',
-  format: combine(
-    label({ label: 'right meow!' }),
-    timestamp(),
-    myFormat,
-    prettyPrint()
-  ),
+  format: combine(label({ label: 'success' }), timestamp(), myFormat),
   transports: [
     new transports.Console(),
-    new transports.File({
-      filename: path.join(process.cwd(), 'logs', 'winston', 'success.log'),
-      level: 'info',
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'logs',
+        'winston',
+        'success-%DATE%.log'
+      ),
+      datePattern: 'YYYY-DD-MM-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
     }),
   ],
 })
 
 const errorLogger = createLogger({
   level: 'error',
-  format: format.json(),
+  format: combine(label({ label: 'error' }), timestamp(), myFormat),
   transports: [
     new transports.Console(),
-    new transports.File({
-      filename: path.join(process.cwd(), 'logs', 'winston', 'error.log'),
-      level: 'error',
+    new DailyRotateFile({
+      filename: path.join(process.cwd(), 'logs', 'winston', 'error-%DATE%.log'),
+      datePattern: 'YYYY-DD-MM-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
     }),
   ],
 })
