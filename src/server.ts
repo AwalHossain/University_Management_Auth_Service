d728b6bb628d370d4399e4510b09f2412a179169
Awal Hossain <80216813+AwalHossain@users.noreply.github.com>
2023-06-12 04:33:19 +0000

    uncaughtException & unhandledRejection handled 👌

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,14 @@ import mongoose from 'mongoose'
 import app from './app'
 import config from './config/index'
 import { errorLogger, logger } from './shared/logger'
+import { Server } from 'http'
+
+process.on('uncaughtException', error => {
+  errorLogger.error(error)
+  process.exit(1)
+})
+
+let server: Server
 
 async function bootstrap() {
   try {
@@ -13,6 +21,18 @@ async function bootstrap() {
   } catch (error) {
     errorLogger.error('error', error)
   }
+
+  process.on('unhandledRejection', error => {
+    if (server) {
+      server.close(() => {
+        errorLogger.error(error)
+        process.exit(1)
+      })
+    } else {
+      errorLogger.error(error)
+      process.exit(1)
+    }
+  })
 }
 
 bootstrap()
