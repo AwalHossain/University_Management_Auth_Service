0ad1284118a325df274440698928ea422e7f634c
Awal Hossain <80216813+AwalHossain@users.noreply.github.com>
2023-06-13 11:07:41 +0000

    academic semester model 🎒

---
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -0,0 +1,37 @@
+import { Schema, model } from 'mongoose'
+import { academicSemesterTitles } from './academicSemester.constant'
+import { IAcademicSemester } from './academicSemester.interface'
+
+const academicSemesterSchema = new Schema<IAcademicSemester>(
+  {
+    title: {
+      type: String,
+      required: true,
+      enum: academicSemesterTitles,
+    },
+    year: {
+      type: Number,
+      required: true,
+    },
+    code: {
+      type: String,
+      required: true,
+    },
+    startMonth: {
+      type: String,
+      required: true,
+    },
+    endMonth: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+export const AcademicSemester = model<IAcademicSemester>(
+  'AcademicSemester',
+  academicSemesterSchema
+)
