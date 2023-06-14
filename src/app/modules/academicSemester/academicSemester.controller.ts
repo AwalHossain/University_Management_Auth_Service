107c7f5417582a5e773b65d81206632c24396537
Awal Hossain <80216813+AwalHossain@users.noreply.github.com>
2023-06-14 07:16:17 +0000

    code refactor ⚒️

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -1,25 +1,28 @@
-import { RequestHandler } from 'express'
+import { NextFunction, Request, RequestHandler, Response } from 'express'
+import httpStatus from 'http-status'
+import catchAsync from '../../../shared/catchAsync'
+import sendResponse from '../../../shared/sendResponse'
+import { IAcademicSemester } from './academicSemester.interface'
 import { AcademicSemesterService } from './academicSemester.service'
 
-const createSemester: RequestHandler = async (req, res, next) => {
-  const { ...academicSemesterData } = req.body
-  // console.log(user, 'user')
+const createSemester: RequestHandler = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { ...academicSemesterData } = req.body
+    // console.log(user, 'user')
 
-  try {
     const result = await AcademicSemesterService.createSemester(
       academicSemesterData
     )
 
-    res.status(201).json({
+    sendResponse<IAcademicSemester>(res, {
+      statusCode: httpStatus.CREATED,
       success: true,
       message: 'Academic semester is created successfully!',
       data: result,
     })
-  } catch (err) {
-    next(err)
+    next()
   }
-}
-
+)
 export const AcademicsSemesterController = {
   createSemester,
 }
