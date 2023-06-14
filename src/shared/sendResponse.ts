4000001dfa968375a174f7cff1194cb3c26d0b0f
Awal Hossain <80216813+AwalHossain@users.noreply.github.com>
2023-06-14 07:07:14 +0000

    catchAsync & sendResponse 🚀🚀

---
+++ src/shared/sendResponse.ts
@@ -0,0 +1,23 @@
+import { Response } from 'express'
+
+type IApiResponse<T> = {
+  statusCode: number
+  success: boolean
+  message?: string | null
+  data?: T | null
+}
+
+const sendResponse = <T>(res: Response, data: IApiResponse<T>): void => {
+  // const { statusCode, success, message } = data;
+
+  const responseData: IApiResponse<T> = {
+    statusCode: data.statusCode,
+    success: data.success,
+    message: data.message || null,
+    data: data.data || null,
+  }
+
+  res.status(data.statusCode).json(responseData)
+}
+
+export default sendResponse
