cb51dc0e4135678dd9ff02bcd219583698868391
Awal Hossain <80216813+AwalHossain@users.noreply.github.com>
2023-06-26 12:09:03 +0000

    management department ✅

---
+++ src/app/modules/admin/admin.model.ts
@@ -0,0 +1,33 @@
+import { Model, Types } from 'mongoose'
+import { UserName } from '../student/student.interface'
+
+export type IAdmin = {
+  id: string
+  name: UserName
+  profileImage: string
+  dateOfBirth?: string
+  email: string
+  contactNo: string
+  emergencyContactNo: string
+  gender?: 'male' | 'female'
+  permanentAddress?: string
+  presentAddress?: string
+  bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-'
+
+  managementDepartment: Types.ObjectId
+  designation: string
+}
+
+export type AdminModel = Model<IAdmin, Record<string, unknown>>
+
+export type IAdminFilters = {
+  searchTerm?: string
+  id?: string
+  email?: string
+  contactNo?: string
+  emergencyContactNo?: string
+  gender?: 'male' | 'female'
+  bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-'
+  managementDepartment?: string
+  designation?: string
+}
