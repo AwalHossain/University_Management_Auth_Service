251f555fd4e3157aaee9f1c1779aea88e3f58726
Awal Hossain <80216813+AwalHossain@users.noreply.github.com>
2023-07-09 23:55:46 +0000

    create student 🚀🚀

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -1,9 +1,15 @@
-import { Model } from 'mongoose'
+import { Model, Types } from 'mongoose'
+import { IAdmin } from '../admin/admin.interface'
+import { IFaculty } from '../faculty/faculty.interface'
+import { IStudent } from '../student/student.interface'
 
 export type Iuser = {
   id: string
   role: string
   password: string
+  student?: Types.ObjectId | IStudent
+  faculty?: Types.ObjectId | IFaculty
+  admin?: Types.ObjectId | IAdmin
 }
 
 export type UserModel = Model<Iuser, Record<string, unknown>>
