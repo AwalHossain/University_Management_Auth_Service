662aa5eceeba63003f338237f1c6ff702f53d145
Awal Hossain <80216813+AwalHossain@users.noreply.github.com>
2023-07-11 09:54:19 +0000

    authentication 🔒

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { Model, Types } from 'mongoose'
 import { IAdmin } from '../admin/admin.interface'
 import { IFaculty } from '../faculty/faculty.interface'
@@ -7,9 +8,21 @@ export type Iuser = {
   id: string
   role: string
   password: string
+  needsPasswordChange: boolean
+  passwordCahngedAt?: Date
   student?: Types.ObjectId | IStudent
   faculty?: Types.ObjectId | IFaculty
   admin?: Types.ObjectId | IAdmin
 }
 
-export type UserModel = Model<Iuser, Record<string, unknown>>
+export type UserModel = {
+  isUserExist(
+    id: string
+  ): Promise<Pick<Iuser, 'id' | 'password' | 'role' | 'needsPasswordChange'>>
+  isPasswordMatched(
+    givenPassword: string,
+    savedPassword: string
+  ): Promise<boolean>
+} & Model<Iuser>
+
+// export type UserModel = Model<Iuser, Record<string, unknown>>
