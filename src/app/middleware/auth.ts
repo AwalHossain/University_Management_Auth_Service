a423521cdb548fd08e4e3a610fbe613ebe4b5c56
Awal Hossain <80216813+AwalHossain@users.noreply.github.com>
2023-07-16 06:07:55 +0000

    user inclueded in Express request object 🚀

---
+++ src/app/middleware/auth.ts
@@ -0,0 +1,38 @@
+import { NextFunction, Request, Response } from 'express'
+import httpStatus from 'http-status'
+import { Secret } from 'jsonwebtoken'
+import config from '../../config'
+import ApiError from '../../errors/ApiError'
+import { jwtHelpers } from '../../helpers/jwtHelpers'
+
+const auth =
+  (...requiredRoles: string[]) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const token = req.headers.authorization
+      if (!token) {
+        throw new ApiError(httpStatus.UNAUTHORIZED, 'Not authorized')
+      }
+
+      // verify token
+      let verifiedUser = null
+
+      verifiedUser = jwtHelpers.verifyToken(token, config.jwt.secret as Secret)
+
+      req.user = verifiedUser // add user to req object
+
+      // use role as authguard
+
+      if (requiredRoles.length) {
+        if (!requiredRoles.includes(verifiedUser.role)) {
+          throw new ApiError(httpStatus.FORBIDDEN, 'Forbidden')
+        }
+      }
+
+      next()
+    } catch (error) {
+      next(error)
+    }
+  }
+
+export default auth
