d551ef593d84f0b32210290af632804d95ed7986
AwalHossain <waqin21@gmail.com>
2023-09-20 12:00:40 +0600

    feat: Add semester, user, faculty & dept routes ➕

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,4 +1,6 @@
 import express from 'express'
+import { AcademicDepartmentRoute } from '../modules/academicDepartment/academicDepartment.route'
+import { AcademicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.route'
 import { AcademicSemesterRoutes } from '../modules/academicSemester/academicSemester.route'
 import { AuthRoutes } from '../modules/auth/auth.route'
 import { UserRoutes } from '../modules/user/user.route'
@@ -11,12 +13,24 @@ const moduleRoutes = [
     route: UserRoutes,
   },
   {
-    path: '/academic-semester',
+    path: '/academic-semesters',
     route: AcademicSemesterRoutes,
+  },
+  {
+    path: '/academic-faculties',
+    route: AcademicFacultyRoutes
   },
   {
     path: '/auth',
     route: AuthRoutes,
+  },
+  {
+    path: '/academic-departments',
+    route: AcademicDepartmentRoute,
+  },
+  {
+    path: '/users',
+    route: UserRoutes,
   },
 ]
 
