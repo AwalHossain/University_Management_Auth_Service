c3b728955deb1b8bc82408a168e41808522337fc
AwalHossain <waqin21@gmail.com>
2023-09-22 15:46:29 +0600

    feat: import cookie parser

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
-import cors from 'cors'
-import express, { Application, Request, Response } from 'express'
-import globalErrorHandler from './app/middleware/globalErrorHandler'
-import routes from './app/routes/index'
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+import globalErrorHandler from './app/middleware/globalErrorHandler';
+import routes from './app/routes/index';
+
+import cookieParser from 'cookie-parser';
+
+
 const app: Application = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
+app.use(cookieParser());
+
 
 console.log(process.env.PORT, 'port')
 
