af0fdf5c1e9713eb250a1a22f1c4a5fc9a49b38f
AwalHossain <waqin21@gmail.com>
2023-09-25 14:10:08 +0600

    fix: bug fixed 🐞

--- src/app/modules/user/user.constant.ts
+++ src/app/modules/user/user.constant.ts
@@ -10,6 +10,6 @@ export const ADMIN_EVENT_UPDATED = 'student_event_updated';
 export const ADMIN_EVENT_DELETED = 'student_event_deleted';
 
 
-export const FACULTY_EVENT_CREATED = 'student_event_created';
-export const FACULTY_EVENT_UPDATED = 'student_event_updated';
-export const FACULTY_EVENT_DELETED = 'student_event_deleted';
+export const FACULTY_EVENT_CREATED = 'faculty_event_created';
+export const FACULTY_EVENT_UPDATED = 'faculty_event_updated';
+export const FACULTY_EVENT_DELETED = 'faculty_event_deleted';
